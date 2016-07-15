d7f7512a4f2f1a2293b10107b00d4d565e30c033
Matthew Hanson <matt.a.hanson@gmail.com>
2016-07-15 01:12:51 -0400

    stub functions

--- modisdownload/core.py
+++ modisdownload/core.py
@@ -1,4 +1,10 @@
 
+
+def get_filename(product, date, tile):
+    """ Get filename for product, date, and tile """
+    # note this will be an obsolete function when there is a modis-api
+    pass
+
 def download(filename):
     """ Download file """
     pass
